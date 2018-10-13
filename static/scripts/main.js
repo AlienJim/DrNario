7cc6ba199b01dd94374515fc3e017549d4f61584
Sking <mrking5218@gmail.com>
2018-10-13 17:29:51 -0400

    Added pixi canvas and moveable sprite

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,68 +1,77 @@
-var host = window.document.location.host.replace(/:.*/, '');
+window.onload = function(){
+    var host = window.document.location.host.replace(/:.*/, '');
+    var client = new Colyseus.Client(location.protocol.replace("http", "ws") + host + (location.port ? ':' + location.port : ''));
+    var room = client.join("pixi");
 
-var client = new Colyseus.Client(location.protocol.replace("http", "ws") + host + (location.port ? ':' + location.port : ''));
-var room = client.join("pixi");
-
-var players = {};
-var colors = ['red', 'green', 'yellow', 'blue', 'cyan', 'magenta'];
-
-// listen to patches coming from the server
-room.listen("players/:id", function(change) {
-  if (change.operation === "add") {
-    var dom = document.createElement("div");
-    dom.className = "player";
-    dom.style.left = change.value.x + "px";
-    dom.style.top = change.value.y + "px";
-    dom.style.background = colors[Math.floor(Math.random() * colors.length)];
-    dom.innerHTML = "Player " + change.path.id;
-
-    players[change.path.id] = dom;
-    document.body.appendChild(dom);
-
-  } else if (change.operation === "remove") {
-    document.body.removeChild(players[ change.path.id ]);
-    delete players[change.path.id];
-  }
-});
-
-room.listen("players/:id/:axis", function(change) {
-  var dom = players[ change.path.id ];
-
-  var styleAttribute = (change.path.axis === "x")
-    ? "left"
-    : "top";
-
-  dom.style[ styleAttribute ] = change.value + "px";
-});
-
-window.addEventListener("keydown", function (e) {
-  console.log(e.which);
-  if (e.which === 38) {
-    up();
-
-  } else if (e.which === 39) {
-    right();
-
-  } else if (e.which === 40) {
-    down();
-
-  } else if (e.which === 37) {
-    left();
-  }
-});
-
-function up () {
-  room.send({ y: -1 });
-}
-
-function right () {
-  room.send({ x: 1 });
-}
-
-function down () {
-  room.send({ y: 1 })
-}
-
-function left () {
-  room.send({ x: -1 })
-}
+    //Creating pixi canvas and fitting it to screen
+    const app = new PIXI.Application((window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth) - 25, 
+              (window.innerHeight
+            || document.documentElement.clientHeight
+            || document.body.clientHeight) - 25);
+    var ele = document.getElementById('canvas');
+    ele.appendChild(app.view);
+    
+    //payers holds sprite pixi sprite
+    var players = {};
+    
+    // listen to patches coming from the server
+    room.listen("players/:id", function(change) {
+      if (change.operation === "add") {
+        var nario = PIXI.Sprite.fromImage('images/drmario.png');
+    
+        nario.x = change.value.x;
+        nario.y = change.value.y;
+    
+        players[change.path.id] = nario;
+        app.stage.addChild(nario);
+    
+      } else if (change.operation === "remove") {
+        //TODO: FIX SPRITE DELETION
+        document.body.removeChild(players[ change.path.id ]);
+        delete players[change.path.id];
+      }
+    });
+    
+    room.listen("players/:id/:axis", function(change) {
+        //moves player upon arrow key press
+        if(change.path.axis === "x"){
+            players[change.path.id].x = change.value;
+        }if(change.path.axis === "y"){
+            players[change.path.id].y = change.value;
+        }
+    });
+    
+    window.addEventListener("keydown", function (e) {
+      console.log("key code: ", e.which);
+      if (e.which === 38) {
+        up();
+    
+      } else if (e.which === 39) {
+        right();
+    
+      } else if (e.which === 40) {
+        down();
+    
+      } else if (e.which === 37) {
+        left();
+      }
+    });
+    
+    function up () {
+      room.send({ y: -1 });
+    }
+    
+    function right () {
+      room.send({ x: 1 });
+    }
+    
+    function down () {
+      room.send({ y: 1 })
+    }
+    
+    function left () {
+      room.send({ x: -1 })
+    }
+ };
