679303f92045de8e48010218019108f46aa06858
Sking <mrking5218@gmail.com>
2018-10-14 14:18:29 -0400

    Reorganized code

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,98 +1,76 @@
-setup = function(){
-  var host = window.document.location.host.replace(/:.*/, '');
-  var client = new Colyseus.Client(location.protocol.replace("http", "ws") + host + (location.port ? ':' + location.port : ''));
-  var room = client.join("pixi");
-  var players = {};
-  var ele = document.getElementById('canvas');
+var host = window.document.location.host.replace(/:.*/, '');
+var client = new Colyseus.Client(location.protocol.replace("http", "ws") + host + (location.port ? ':' + location.port : ''));
+var room = client.join("pixi");
+var players = {};
 
-  function getWindowX(){
-    return ( window.innerWidth
-          || document.documentElement.clientWidth
-          || document.body.clientWidth) - 25;
-  }
-  function getWindowY(){
-    return ( window.innerHeight
-          || document.documentElement.clientHeight
-          || document.body.clientHeight) - 25;
-  }
+// listen to patches coming from the server
+room.listen("players/:id", function(change) {
+  if (change.operation === "add") {
+    mySprites.app.stage.addChild(mySprites.marios[0]);
 
-  //Creating pixi canvas and fitting it to screen
-  const app = new PIXI.Application(getWindowX(), getWindowY());
-  
-  ele.appendChild(app.view);
-  var pills = document.mySprites.createSprite();
-  var z = 0;
-  pills.forEach(function(element) {
-      element.y = z;
-      app.stage.addChild(element);
-      z += 40;
-  });
-  
-  // listen to patches coming from the server
-  room.listen("players/:id", function(change) {
-    if (change.operation === "add") {
-      var nario = PIXI.Sprite.fromImage('images/drmario.png');
-  
-      nario.x = change.value.x;
-      nario.y = change.value.y;
-  
-      players[change.path.id] = nario;
-      app.stage.addChild(nario);
-  
-    } else if (change.operation === "remove") {
-      //TODO: FIX SPRITE DELETION
-      ele.removeChild(players[change.path.id]);
-      delete players[change.path.id];
+    mySprites.marios[0].x = change.value.x;
+    mySprites.marios[0].x = change.value.y;
+
+    players[change.path.id] = mySprites.marios[0];
+
+    //test to display pills
+    /*
+    var z = 0;
+    mySprites.dubPills.forEach(function(element) {
+        mySprites.app.stage.addChild(element);
+        element.y += z;
+        console.log("does this work");
+        z += 40;
+    });
+    */
+  } else if (change.operation === "remove") {
+    //TODO: FIX SPRITE DELETION
+    mySprites.ele.removeChild(players[change.path.id]);
+    delete players[change.path.id];
+  }
+});
+
+room.listen("players/:id/:axis", function(change) {
+    //moves player upon arrow key press
+    if(change.path.axis === "x"){
+        players[change.path.id].x = change.value;
+    }if(change.path.axis === "y"){
+        players[change.path.id].y = change.value;
     }
-  });
-  
-  room.listen("players/:id/:axis", function(change) {
-      //moves player upon arrow key press
-      if(change.path.axis === "x"){
-          players[change.path.id].x = change.value;
-      }if(change.path.axis === "y"){
-          players[change.path.id].y = change.value;
-      }
-  });
+});
 
-  window.addEventListener("keydown", function (e) {
-    console.log("key code: ", e.which);
-    if (e.which === 38) {
-      up();
-  
-    } else if (e.which === 39) {
-      right();
-  
-    } else if (e.which === 40) {
-      down();
-  
-    } else if (e.which === 37) {
-      left();
-    }
-  });
-  
-  function up () {
-    room.send({ y: -1 });
+window.addEventListener("keydown", function (e) {
+  console.log("key code: ", e.which);
+  if (e.which === 38) {
+    up();
+
+  } else if (e.which === 39) {
+    right();
+
+  } else if (e.which === 40) {
+    down();
+
+  } else if (e.which === 37) {
+    left();
   }
-  
-  function right () {
-    room.send({ x: 1 });
-  }
-  
-  function down () {
-    room.send({ y: 1 })
-  }
-  
-  function left () {
-    room.send({ x: -1 })
-  }
-  
-  //event listener to auto resize window
-  window.addEventListener("resize", () => {   
-      app.renderer.resize(getWindowX(), getWindowY());
-  })
-  return app;
-};
-PIXI.loader
-  .add("images/bgsheet.png")
-  .load(setup);
+});
+
+function up () {
+  room.send({ y: -1 });
+}
+
+function right () {
+  room.send({ x: 1 });
+}
+
+function down () {
+  room.send({ y: 1 })
+}
+
+function left () {
+  room.send({ x: -1 })
+}
+//event listener to auto resize window
+window.addEventListener("resize", () => {   
+    app.renderer.resize(getWindowX(), getWindowY());
+})
--- static/scripts/sprites.js
+++ static/scripts/sprites.js
@@ -1,8 +1,23 @@
-document.mySprites = {
-    createSprite : function createSprite(){
+const app = new PIXI.Application(getWindowX(), getWindowY());
+var elem;
+var mySprites = {
+    app: app,
+    ele: elem,
+    dubPills: [],
+    marios: []
+};
+
+window.onload = function(){
+    //Creating pixi canvas and fitting it to screen
+    elem = document.getElementById('canvas');
+    elem.appendChild(app.view);
+
+    PIXI.loader
+            .add("images/bgsheet.png")
+            .load(setup);
+    function setup(){
         var texture = PIXI.utils.TextureCache['images/bgsheet.png'];
         var x = 12, y = 135, width = 19, height = 11;
-        var dubPills = [];
 
         for(var i= 0; i < 3; i++){
             for(var w = 0; w < 3; w++){
@@ -13,57 +28,78 @@ document.mySprites = {
                 //newTexture._updateUvs();
 
                 let sprite = new PIXI.Sprite(newTexture);
-                dubPills.push(sprite);
-                console.log(sprite.texture, newTexture.frame);
+                mySprites.dubPills.push(sprite);
             }
         }
-        return dubPills;
 
-        /*
-        sprite positions and size
-        aliens:
-        (x,y)
-            Alien size:
-                24 x 24 all
-            Blue:
-                1: (274, 44)
-                            3px between ^V
-                2: (301, 44)
-                            3px between ^V
-                3: (328, 44)
-                            5px between ^V
-                4: (357, 44)
-                            5px between ^V
-                5: (386, 44)
-                            6px between ^V
-                6: (416, 44)
-                            ^^These values are the same for all
-            yellow:
-                1: (274, 73)
-                2: (301, 73)
-                3: (328, 73)
-                4: (357, 73)
-                5: (386, 73)
-                6: (416, 73)
-            red:
-                1: (274, 102)
-                2: (301, 102)
-                3: (328, 102)
-                4: (357, 102)
-                5: (386, 102)
-                6: (416, 102)
+        let newTexture = new PIXI.Texture(texture.baseTexture, texture.frame);
+        let rectangle = new PIXI.Rectangle(15, 48, 24, 40);
 
-            height is 9px down from bottom of prev sprite to top of next sprite
-        pills:
-            double-pill size:
-                19 x 11
-            single-pill size:
-                11 x 11
-                start:
-                    (12, 135)
-                    each pill is evenly divided 3 by 3
+        newTexture.frame = rectangle;
 
-        //TODO: the rest
-        */
+        let sprite = new PIXI.Sprite(newTexture);
+        mySprites.marios.push(sprite);
+        //console.log(sprite.texture, newTexture.frame);
     }
-};
+}
+function getWindowX(){
+    return (window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth) - 25;
+}
+function getWindowY(){
+    return (window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight) - 25;
+}
+            
+
+/*
+    sprite positions and size
+    aliens:
+    (x,y)
+        Alien size:
+            24 x 24 all
+        Blue:
+            1: (274, 44)
+                        3px between ^V
+            2: (301, 44)
+                        3px between ^V
+            3: (328, 44)
+                        5px between ^V
+            4: (357, 44)
+                        5px between ^V
+            5: (386, 44)
+                        6px between ^V
+            6: (416, 44)
+                        ^^These values are the same for all
+        yellow:
+            1: (274, 73)
+            2: (301, 73)
+            3: (328, 73)
+            4: (357, 73)
+            5: (386, 73)
+            6: (416, 73)
+        red:
+            1: (274, 102)
+            2: (301, 102)
+            3: (328, 102)
+            4: (357, 102)
+            5: (386, 102)
+            6: (416, 102)
+
+        height is 9px down from bottom of prev sprite to top of next sprite
+    pills:
+        double-pill size:
+            19 x 11
+        single-pill size:
+            11 x 11
+            start:
+                (12, 135)
+                each pill is evenly divided 3 by 3
+    dr mario:
+        24X40
+            1: (15, 48)
+        
+    //TODO: the rest
+*/
